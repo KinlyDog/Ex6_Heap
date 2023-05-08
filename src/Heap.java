195f9b72abfc05574b422f53c282f2a25d9e7a4d
KinlyDog <kinlydog@gmail.com>
2023-05-09 01:38:39 +1000

    Update project

--- src/Heap.java
+++ src/Heap.java
@@ -2,25 +2,120 @@ import java.util.*;
 
 class Heap {
     public int[] HeapArray; // хранит неотрицательные числа-ключи
+    int end;
 
     public Heap() {
         HeapArray = null;
+        end = -1;
+    }
+
+    private int heapSize(int depth) {
+        int treeSize = 0;
+
+        while (depth >= 0) {
+            treeSize += (int) Math.pow(2, depth--);
+        }
+
+        return treeSize;
     }
 
     public void MakeHeap(int[] a, int depth) {
-        // создаём массив кучи HeapArray из заданного
-        // размер массива выбираем на основе глубины depth
-        // ...
+        int size = heapSize(depth);
+
+        HeapArray = new int[size];
+        for (int i = 0; i < a.length; i++) {
+            Add(a[i]);
+        }
+
+        for (int i = a.length; i < size; i++) {
+            HeapArray[i] = -1;
+        }
+
+        end = a.length - 1;
     }
 
     public int GetMax() {
-        // вернуть значение корня и перестроить кучу
-        return -1; // если куча пуста
+        if (HeapArray == null || HeapArray[0] == -1) {
+            return -1;
+        }
+
+        int max = HeapArray[0];
+        HeapArray[0] = HeapArray[end];
+        HeapArray[end--] = -1;
+        resort(0);
+
+        return max;
+    }
+
+    private void resort(int i) {
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild >= HeapArray.length || rightChild >= HeapArray.length) {
+            return;
+        }
+
+        int max = HeapArray[leftChild] > HeapArray[rightChild] ? leftChild : rightChild;
+
+        if (HeapArray[max] < HeapArray[i]) {
+            return;
+        }
+
+        int temp = HeapArray[i];
+        HeapArray[i] = HeapArray[max];
+        HeapArray[max] = temp;
+
+        i = max;
+
+        resort(i);
     }
 
     public boolean Add(int key) {
-        // добавляем новый элемент key в кучу и перестраиваем её
-        return false; // если куча вся заполнена
+        if (HeapArray == null || end == HeapArray.length - 1) {
+            return false;
+        }
+
+        if (HeapArray[0] == -1) {
+            HeapArray[0] = key;
+            end++;
+            return true;
+        }
+
+        HeapArray[++end] = key;
+
+        AddRec(end);
+
+        return true;
     }
 
+    private void AddRec(int i) {
+        if (i <= 0) {
+            return;
+        }
+
+        int parent = (i - 1) / 2;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if ((leftChild >= HeapArray.length ||
+                rightChild >= HeapArray.length) &&
+                HeapArray[i] < HeapArray[parent]) {
+            return;
+        }
+
+        if (HeapArray[i] < HeapArray[parent] &&
+                HeapArray[i] > HeapArray[leftChild] &&
+                HeapArray[i] > HeapArray[rightChild]) {
+            return;
+        }
+
+        int temp = HeapArray[parent];
+
+        HeapArray[parent] = HeapArray[i];
+        HeapArray[i] = temp;
+
+        i = parent;
+
+        AddRec(i);
+    }
 }
